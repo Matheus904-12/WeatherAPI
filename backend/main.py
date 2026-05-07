4dd34663ae8f5295547d20d470fbb7487b311c2e
Matheus Lucindo <matheuslucindo904@gmail.com>
2026-05-07 19:50:09 -0300

    feat: Implementação dos testes unitarios e de CI & CD

--- backend/main.py
+++ backend/main.py
@@ -25,10 +25,12 @@ app = FastAPI(
     description="Um serviço de clima com Arquitetura Hexagonal",
     version="1.0.0"
 )
-
+# CORS
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "http://localhost:5174",
+    "http://127.0.0.1:5174",
 ]
 
 app.add_middleware(
